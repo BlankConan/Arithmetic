602288c4c33f5d05ad7fe681c98edf67e5abbd87
liugangyi <liugangyi@liugangyi.local>
2019-01-10 19:09:22 +0800

    测试分支问题

---
+++ Arithmetic/Classes/Num/TestBranch.swift
@@ -0,0 +1,13 @@
+//
+//  TestBranch.swift
+//  Arithmetic
+//
+//  Created by liugangyi on 2019/1/10.
+//  Copyright © 2019年 liu gangyi. All rights reserved.
+//
+
+import Cocoa
+
+class TestBranch: NSObject {
+
+}
